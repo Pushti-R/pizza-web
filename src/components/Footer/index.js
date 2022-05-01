2fa6519cfb99a0df4e78c2fb29cd660d9f5bdf23
Pushti <pushti.ratanghayra@gmail.com>
2022-05-01 11:41:42 +0530

    C1

---
+++ src/components/Footer/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>Pizza</SocialLogo>
+            <SocialIcons>
+              <SocialIconLink href='/'  aria-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' aria-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' aria-label='Youtube'>
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href='/' aria-label='Twitter'>
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' aria-label='Linkedin'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
